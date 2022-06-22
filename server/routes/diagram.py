b1eaf1fc3b48dceaedca50d6fcd6f7add19f2c3f
Jordan Conklin <jordan.conklin@ibm.com>
2022-06-22 15:08:07 -0500

    some changes to orientation and formatting
    Signed-off-by: Jordan Conklin <jordan.conklin@ibm.com>

--- server/routes/diagram.py
+++ server/routes/diagram.py
@@ -3,7 +3,7 @@ from crypt import methods
 from flask import send_file
 from server import app
 
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import ECS, EKS, Lambda
 from diagrams.aws.database import Redshift
 from diagrams.aws.integration import SQS
@@ -18,26 +18,34 @@ from diagrams.gcp.storage import GCS
 def diagram():
     """diagram route"""
 
-    with Diagram("IBM Cloud", show=False, filename='/tmp/diagram'):
-
-        with Cluster("quick start (VPC)"):
-            with Cluster("ACL: worker"):
-                with Cluster("Zone 1 - 10.1.0.0/22"):
-                    worker1 = ECS("Worker 1")
+    with Diagram( show=False, filename='/tmp/diagram'):
+        with Cluster("IBM Cloud"):
+            with Cluster("quick start (VPC)"):
+                with Cluster("ACL: worker"):
+                    with Cluster("Zone 1 - 10.1.0.0/22"):
+                        worker1 = ECS("Worker 1")
+                    
+                    with Cluster("Zone 2 - 10.2.0.0/22"):
+                        worker2 = ECS("Worker 2")
+                    
+                    with Cluster("Zone 3 - 10.3.0.0/22"):
+                        worker3 = ECS("Worker 3")
+                    
                 
-                with Cluster("Zone 2 - 10.2.0.0/22"):
-                    worker2 = ECS("Worker 2")
-                
-                with Cluster("Zone 3 - 10.3.0.0/22"):
-                    worker3 = ECS("Worker 3")
-                
+            with Cluster("Cloud Services"):
+                handlers = [BigQuery("Monitoring"), 
+                            GCS("Logging"), 
+                            Redshift("Analytics")]
             
 
-        with Cluster("Cloud Services"):
-            handlers = [BigQuery("Monitoring"), 
-                        GCS("Logging"), 
-                        Redshift("Analytics")]
+            worker1 \
+                - Edge(color="red", style="dashed") \
+                - worker2 \
+                - Edge(color="red", style="dashed") \
+                - worker3 \
+                - Edge(color="blue", style="dashed") \
+                - handlers[1]
+        
 
-        worker1 >> worker2 >> worker3
 
     return send_file('/tmp/diagram.png', mimetype='image/png')
