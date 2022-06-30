93c0f18d6a21ecfd5f0f452c7f41558784d07a98
Jordan Conklin <jordan.conklin@ibm.com>
2022-06-30 14:18:31 -0500

    got diagram looking nice, less generic and more fitting to the product. ibm-standard is not as full as the quickstart, but quickstart is pretty good
    Signed-off-by: Jordan Conklin <jordan.conklin@ibm.com>

--- server/routes/diagram.py
+++ server/routes/diagram.py
@@ -1,9 +1,11 @@
 
 from crypt import methods
+from curses.ascii import ETB
 from doctest import master
 from hashlib import new
 from itertools import count
 from re import I
+from telnetlib import EC
 from flask import send_file
 from server import app
 
@@ -16,18 +18,18 @@ from diagrams.aws.storage import S3
 from diagrams.gcp.analytics import BigQuery
 from diagrams.gcp.storage import GCS
 
-
 import json
+
+
+
 
 
 with open("./public/ibm-standard.json", "r") as stream:
     file = (json.load(stream))
-
 stream.close()
 
-if (file["bom"]["metadata"]["labels"]["platform"] == "ibm"):
-    print(file["bom"]["metadata"]["labels"]["platform"])
-    # some logic for using ibm icons
+ignored = ["ibm-resource-group", "ibm-access-group", "ibm-vpc-gateways", "ibm-log-analysis-bind", "ibm-cloud-monitoring-bind", "kms-key"]
+platservice = ["ibm-log-analysis", "ibm-cloud-monitoring", "ibm-activity-tracker", "ibm-object-storage", "cos"]  # list of cloud services
 
 
 @app.route("/diagram")
@@ -35,43 +37,118 @@ def diagram():
     """diagram route"""
     
     with Diagram( show=False, filename='/tmp/diagram'):
-        # have a dict of dependencies, key is the name and val is the list of nodes
-        clusters = {}
-        # iterate through modules
-        for i in file["bom"]["spec"]["modules"]:
-            d = True
-            # go through each item in the module
-            for key, val in i.items():
-                # for each module, get the name and dependencies
-                if key == "name":
-                    name = val
-                if key == "alias":
-                    name = val
-                if key == "dependencies":
-                    d = False
-                    # loop through each dependency
-                    for i in val:
-                        s = str(val[0]["ref"])
-                        if s in clusters.keys():
-                            clusters[s].append(name)
-                        else:
-                            clusters[s] = [name]
-                if d and key == "enrichedMetadata":
-                    # loop through each dependency
-                    for i in val["dependencies"]:
-                        s = str(i["id"])
-                        if s in clusters.keys():
-                            clusters[s].append(name)
-                        else:
-                            clusters[s] = [name]
+        with Cluster(file["bom"]["metadata"]["labels"]["platform"]):
+            # have a dict of dependencies, key is the name and val is the list of nodes
+            clusters = {"ibm-vpc-subnets": [3, [0]], "ibm-ocp-vpc": [0, [0]], 
+                        "worker-subnets": [0, [0]], "vpe-subnets": [0, [0]]} # last two are for standard
+            services = []
+            # iterate through modules
+            for i in file["bom"]["spec"]["modules"]:
+                d = True
+                v = True
+                # go through each item in the module
+                for key, val in i.items():
+                    # for each module, get the name and dependencies
+                    if key == "name":
+                        name = val
+                        if name in ignored:
+                            break
+                    if key == "alias" and "-subnets" in val:
+                        name = val
+                    
+                    if name in platservice:
+                        # new node in cloud service cluster
+                        services.append(name)
+                        break
 
-        # create new cluster for each key
-        for clust in clusters.keys():
-            makeClusters(clusters, clust)
+                    if name in clusters.keys() and key == "variables":
+                        v = False
+                        # for each variable:
+                        for var in val:
+                            if var["name"] == "worker_count" or var["name"] == "_count":
+                                clusters[name][0] = var["value"]
+                            if var["name"] == "ipv4_cidr_blocks":
+                                clusters[name][1] = var["value"]
+
+                    # if key == "dependencies":
+                    #     d = False
+                    #     # loop through each dependency
+                    #     for i in val:
+                    #         s = str(val[0]["ref"])
+                    #         if s in clusters.keys():
+                    #             clusters[s].append(name)
+                    #         else:
+                    #             clusters[s] = [name]
+
+                    if key == "enrichedMetadata":
+                        if v:
+                            # get num subnets and num worker nodes
+                            if name in clusters.keys():
+                                for i in val["variables"]:
+                                    if i["name"] == "worker_count" or i["name"] == "_count":
+                                        num = int(i["default"])
+                                        clusters[name][0] =  num
+                                        break
+
+                        if d:
+                            # loop through each dependency
+                            for depend in val["dependencies"]:
+                                s = str(depend["id"])
+                                if s in ignored:
+                                    break
+                                if s in platservice and "ibm-object-storage" not in services:
+                                    services.append(s)
+                                    continue
+                                # if s in clusters.keys():
+                                #     clusters[s].append(name)
+                                # else:
+                                #     clusters[s] = [name]
+
+
+            nodes = {}
+            with Cluster("Cloud Services"):
+                for serv in services:
+                    n = GCS(serv)
+                    nodes.update({serv: n})
+            
+            with Cluster("VPC"):
+                if clusters["worker-subnets"][0] > 0:
+                    for net in range(1, clusters["worker-subnets"][0] + 1):
+                        with Cluster("Zone " + str(net)):
+                            for worker in range(0, clusters["ibm-ocp-vpc"][0]):
+                                s = str(clusters["worker-subnets"][1][worker]) + " - worker"+str(net)
+                                n = ECS(s)
+                                nodes.update({"worker"+str(net): n})
+                            
+                    for net in range(1, clusters["vpe-subnets"][0] + 1):
+                        with Cluster("Zone " + str(net)):
+                            for worker in range(0, clusters["ibm-ocp-vpc"][0]):
+                                s = str(clusters["vpe-subnets"][1][worker]) + " - vpe"+str(net)
+                                n = ECS(s)
+                                nodes.update({"vpe"+str(net): n})
+                    nodes["vpe1"] - Edge(color="red") - nodes["vpe2"] - Edge(color="red") - nodes["vpe3"] \
+                        - Edge(color="blue", style="dashed") - nodes["ibm-log-analysis"]
+                            
+                else:
+                    for net in range(1, clusters["ibm-vpc-subnets"][0] + 1):
+                        with Cluster("Zone " + str(net)):
+                            for worker in range(0, clusters["ibm-ocp-vpc"][0]):
+                                s = str(clusters["ibm-ocp-vpc"][1][worker]) + " - worker"+str(net)
+                                n = ECS(s)
+                                nodes.update({"worker"+str(net): n})
+                    nodes["worker3"] - Edge(color="blue", style="dashed") - nodes["ibm-log-analysis"]
+ 
+            nodes["worker1"] - Edge(color="red") - nodes["worker2"] - Edge(color="red") - nodes["worker3"]
+
+            # create new cluster for each key
+            # for clust in clusters.keys():
+            #     makeClusters(clusters, clust)
+
 
     return send_file('/tmp/diagram.png', mimetype='image/png')
 
 
+# not being used right now
 def makeClusters(clusters, clust):
     with Cluster(clust):
         # include each node that belongs in the cluster
@@ -89,31 +166,3 @@ def makeClusters(clusters, clust):
             nodes[i] - Edge(color="blue") - nodes[i + 1]
     return 1
 
-
-
-
-
-# hard code from before for reference:
-
-# with Cluster("quick start (VPC)"):
-    #     with Cluster("ACL: worker"):
-    #         with Cluster("Zone 1 - 10.1.0.0/22"):
-    #             worker1 = ECS("Worker 1")
-            
-    #         with Cluster("Zone 2 - 10.2.0.0/22"):
-    #             worker2 = ECS("Worker 2")
-            
-    #         with Cluster("Zone 3 - 10.3.0.0/22"):
-    #             worker3 = ECS("Worker 3")
-
-    # with Cluster("Cloud Services"):
-    #     handlers = [BigQuery("Monitoring"), 
-    #                 GCS("Logging"), 
-    #                 Redshift("Analytics")]
-    # worker1 \
-    #     - Edge(color="red", style="dashed") \
-    #     - worker2 \
-    #     - Edge(color="red", style="dashed") \
-    #     - worker3 \
-    #     - Edge(color="blue", style="dashed") \
-    #     - handlers[0]
