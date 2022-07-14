c9deccf152435542f678d7c0674bb7b53328466c
Jordan Conklin <jordan.conklin@ibm.com>
2022-07-14 18:12:46 -0500

    ibm and aws diagrams implemented
    Signed-off-by: Jordan Conklin <jordan.conklin@ibm.com>

--- server/routes/diagram.py
+++ server/routes/diagram.py
@@ -1,3 +1,8 @@
+import json
+import re
+
+from diagrams import *
+
 from crypt import methods
 from curses.ascii import ETB
 from doctest import master
@@ -5,57 +10,62 @@ from hashlib import new
 from itertools import count
 from re import I
 from telnetlib import EC
+from urllib.request import urlretrieve
 from flask import send_file
 from server import app
 
-from diagrams import Cluster, Diagram, Edge
-
-from diagrams.aws.compute import ECS, EKS, Lambda
+from diagrams.aws.compute import ECS, EKS, Lambda, EC2
 from diagrams.aws.database import Redshift
 from diagrams.aws.integration import SQS
 from diagrams.aws.storage import S3
+from diagrams.aws.security import KMS
+from diagrams.aws.analytics import Redshift
 
-from diagrams.ibm.general import Monitoring, MonitoringLogging, Cloudant, IotCloud
-from diagrams.ibm.data import Cloud
-from diagrams.ibm.storage import ObjectStorage
-from diagrams.ibm.infrastructure import Diagnostics
-from diagrams.ibm.network import InternetServices, VpnConnection, Router, Bridge, DirectLink, Gateway
-from diagrams.ibm.management import DeviceManagement
-from diagrams.ibm.user import User, Browser
-from diagrams.ibm.compute import Key
-from diagrams.ibm.social import FileSync
-from diagrams.ibm.applications import EnterpriseApplications
+from diagrams.ibm.general import *
+from diagrams.ibm.logging import *
+from diagrams.ibm.storage import ObjectStorage, FileStorage
+from diagrams.ibm.network import *
+from diagrams.ibm.compute import PowerSystems
 
 
-import json
 
-
-# change type of diagram here
-type = "ibm-edge"
+# change type of diagram here via json file name i.e. 'ibm-production' or 'ibm-quickstart'
+type = "aws-standard"
 with open("./public/"+type+".json", "r") as stream:
     file = (json.load(stream))
 stream.close()
 
 # ibm specific node icons
 ignored = ["ibm-resource-group", "ibm-access-group", "ibm-vpc-gateways", "ibm-log-analysis-bind", "ibm-cloud-monitoring-bind", "kms-key"]
-platservice = {"ibm-log-analysis": MonitoringLogging, "ibm-cloud-monitoring": Monitoring, "ibm-activity-tracker":Diagnostics, 
-                "ibm-object-storage":ObjectStorage, "cos":ObjectStorage}  # dict of cloud services
 
-workernode = Key  # Cloud, Router
-vpenode = VpnConnection
-ingressnode = Bridge
-bastionnode = DeviceManagement
-egressnode = Cloud
+# pre-defined nodes for diagrams
+
+icons ={"ibm" : {"worker":PowerSystems, "vpe":VpcEndpoints, 
+        "ingress":Subnets, "bastion":VirtualRouterAppliance, "egress":VirtualRouterAppliance},
+        
+        "aws" : {"rosa":Redshift, "bastion":EC2, "instance":EC2}}
+
+platservice = {"ibm-log-analysis":LogAnalysis, "ibm-cloud-monitoring":Monitoring, "ibm-activity-tracker":ActivityTracker, 
+               "ibm-object-storage":ObjectStorage, "cos":ObjectStorage, 
+               "aws-kms":KMS}  # dict of cloud services
+
+
 
 @app.route("/diagram")
 def diagram():
+    
     """diagram route"""
+    global emplink, conslink, dirlink, servlink
     
     with Diagram( show=False, filename='/tmp/diagram'):
-        with Cluster(file["bom"]["metadata"]["labels"]["platform"] + " Cloud"):
+        pform = type.split("-", 1)[0]  # get the platform i.e. aws, ibm, azure
+        print(pform)
+        with Cluster(pform + " Cloud"):
             # have a dict of dependencies, key is the name and val is the list of nodes
-            clusters = {"ibm-vpc-subnets": [3, [0]], "ibm-ocp-vpc": [0, [0]], 
-                        "worker-subnets": [0, [0]], "vpe-subnets": [0, [0]], "ingress-subnets": [0, [0]], "bastion-subnets": [0, [0]], "egress-subnets": [0, [0]]} 
+            clusters = {"ibm-ocp-vpc": [0, [0]], "worker-subnets": [0, [0]], "vpe-subnets": [0, [0]], 
+                        "ingress-subnets": [0, [0]], "bastion-subnets": [0, [0]], "egress-subnets": [0, [0]],
+                        "aws-vpc": [0, [0]], "pri_subnets": [0, [0], ''], "pub_subnets": [0, [0], '']} 
+            awsnodes = ["aws-rosa", "aws-ec2-bastion", "aws-ec2-instance"]
             services = []
             # iterate through modules
             for i in file["bom"]["spec"]["modules"]:
@@ -68,7 +78,7 @@ def diagram():
                         name = val
                         if name in ignored:
                             break
-                    if key == "alias" and "-subnets" in val:
+                    if key == "alias" and ("-subnets" in val or val in clusters.keys()):
                         name = val
                     
                     if name in platservice.keys():
@@ -77,26 +87,22 @@ def diagram():
                         break
 
                     if name in clusters.keys() and key == "variables":
-                        
                         # for each variable:
                         for var in val:
                             if (var["name"] == "worker_count" or var["name"] == "_count") and "value" in var:
                                 v = False
                                 clusters[name][0] = var["value"]
-                            if var["name"] == "ipv4_cidr_blocks":
+                            if var["name"] == "ipv4_cidr_blocks" or var["name"] == "subnet_cidrs":
                                 v = False
                                 clusters[name][1] = var["value"]
+                                clusters[name][0] = len(clusters[name][1])
 
-                    # if key == "dependencies":
-                    #     d = False
-                    #     # loop through each dependency
-                    #     for i in val:
-                    #         s = str(val[0]["ref"])
-                    #         if s in clusters.keys():
-                    #             clusters[s].append(name)
-                    #         else:
-                    #             clusters[s] = [name]
-
+                    if name in awsnodes and key == "dependencies":
+                        for dep in val:
+                            if 'ref' in dep.keys() and dep['ref'] in clusters.keys():
+                                print(dep['ref'])
+                                clusters[dep['ref']][2] = name.rsplit("-", 1)[1]
+                    
                     if key == "enrichedMetadata":
                         if v:
                             # get num subnets and num worker nodes
@@ -116,135 +122,91 @@ def diagram():
                                 if s in platservice.keys() and "ibm-object-storage" not in services:
                                     services.append(s)
                                     continue
-                                # if s in clusters.keys():
-                                #     clusters[s].append(name)
-                                # else:
-                                #     clusters[s] = [name]
 
-        
+
+            print(clusters)
             nodes = {}
-            if type != "ibm-edge":
-                with Cluster("Cloud Services"):
-                    if "cos" in services and "ibm-object-storage" in services:
-                        services.remove("cos")
-                    for serv in services:
-                        n = platservice[serv](serv)
-                        nodes.update({serv: n})
-            
-            with Cluster(type + " (VPC)"):
-                if clusters["worker-subnets"][0] > 0: 
-                    for net in range(1, clusters["worker-subnets"][0] + 1):
-                        with Cluster("Zone " + str(net)):
-                            s = str(clusters["worker-subnets"][1][net - 1]) + " - worker"+str(net)
-                            n = workernode(s)
-                            nodes.update({"worker"+str(net): n})
-                    nodes["worker1"] - Edge(color="red") - nodes["worker2"] - Edge(color="red") - nodes["worker3"] 
-                    
-                    conslink = nodes["worker1"]
-                    dirlink = nodes["worker3"]
 
-                elif clusters["ibm-ocp-vpc"][0] > 0:
-                    for net in range(1, clusters["ibm-vpc-subnets"][0] + 1):
+            with Cluster(type + " (VPC)"):
+                if clusters["worker-subnets"][0] > 0 and clusters["ibm-ocp-vpc"][0] > 0:
+                    clusters["ibm-ocp-vpc"][0] = 0
+                    
+                if type == 'ibm-quickstart':
+                    for net in range(1, 4):
                         with Cluster("Zone " + str(net)):
                             s = "worker"+str(net)
-                            n = workernode(s)
+                            n = icons["ibm"]["workernode"](s)
                             nodes.update({"worker"+str(net): n})
                     nodes["worker1"] - Edge(color="red") - nodes["worker2"] - Edge(color="red") - nodes["worker3"] \
                         >> nodes["ibm-log-analysis"]
+
+                else:
+                    for key in clusters.keys():
+                        if clusters[key][0] > 0:
+                            for net in range(1, clusters[key][0] + 1):
+                                with Cluster("Zone " + str(net)):
+                                    b = re.split('-|_', key)[0]
+                                    s = str(clusters[key][1][net - 1]) + "\n" + b + str(net)  # "worker1"
+                                    if 'pub' in b or 'pri' in b:
+                                        with Cluster(s):
+                                            n = icons[pform][clusters[key][2]](clusters[key][2])
+                                            b = clusters[key][2]  # would be something like rosa
+                                    else:
+                                        n = icons[pform][b](s)
+                                    nodes.update({b + str(net): n})
+                                if net > 1:  # past first zone
+                                    nodes[b + str(net - 1)] - Edge(color="red") - n
+
+                    if "worker1" in nodes.keys():
+                        conslink = nodes["worker1"]
+                    elif "ingress1" in nodes.keys():
+                        conslink = nodes["ingress1"]
+                    elif "rosa1" in nodes.keys():
+                        conslink = nodes["rosa1"]
+                    elif "instance1" in nodes.keys():
+                        conslink = nodes["instance1"]
                     
+                    if "worker3" in nodes.keys():
+                        dirlink = nodes["worker3"]
+                        servlink = nodes["worker3"]
+                    elif "ingress3" in nodes.keys():
+                        dirlink = nodes["ingress3"]
+                        servlink = nodes["ingress3"]
+                    elif "bastion3" in nodes.keys():
+                        dirlink = nodes["bastion3"]
+                        servlink = nodes["bastion3"]
 
-                if clusters["vpe-subnets"][0] > 0:
-                    for net in range(1, clusters["vpe-subnets"][0] + 1):
-                        with Cluster("Zone " + str(net)):
-                            s = str(clusters["vpe-subnets"][1][net - 1]) + " - vpe"+str(net)
-                            n = vpenode(s)
-                            nodes.update({"vpe"+str(net): n})
-                    nodes["vpe1"] - Edge(color="red") - nodes["vpe2"] - Edge(color="red") - nodes["vpe3"] \
-                        >> Gateway("VPE Gateway") >> nodes["ibm-log-analysis"]
-                    
-                    emplink = nodes["vpe1"]
+                    if "vpe1" in nodes.keys():
+                        emplink = nodes["vpe1"]
+                    elif "ingress1" in nodes.keys():
+                        emplink = nodes["ingress1"]
+                    elif "bastion1" in nodes.keys():
+                        emplink = nodes["bastion1"]
 
-
-                if clusters["ingress-subnets"][0] > 0:
-                    for net in range(1, clusters["ingress-subnets"][0] + 1):
-                        with Cluster("Zone " + str(net)):
-                            s = str(clusters["ingress-subnets"][1][net - 1]) + " - ingress"+str(net)
-                            n = ingressnode(s)
-                            nodes.update({"ingress"+str(net): n})
-                    nodes["ingress1"] - Edge(color="red") - nodes["ingress2"] - Edge(color="red") - nodes["ingress3"]
-                    
-                    conslink = nodes["ingress1"]
-                    emplink = nodes["ingress1"]
-                    dirlink = nodes["ingress3"]
-                
-
-                if clusters["bastion-subnets"][0] > 0:
-                    for net in range(1, clusters["bastion-subnets"][0] + 1):
-                        with Cluster("Zone " + str(net)):
-                            s = str(clusters["bastion-subnets"][1][net - 1]) + " - bastion"+str(net)
-                            n = bastionnode(s)
-                            nodes.update({"bastion"+str(net): n})
-                    nodes["bastion1"] - Edge(color="red") - nodes["bastion2"] - Edge(color="red") - nodes["bastion3"] 
-                
-
-                if clusters["egress-subnets"][0] > 0:
-                    for net in range(1, clusters["egress-subnets"][0] + 1):
-                        with Cluster("Zone " + str(net)):
-                            s = str(clusters["egress-subnets"][1][net - 1]) + " - egress"+str(net)
-                            n = egressnode(s)
-                            nodes.update({"egress"+str(net): n})
-                    nodes["egress1"] - Edge(color="red") - nodes["egress2"] - Edge(color="red") - nodes["egress3"]
-                
-
-        # print(clusters["worker-subnets"])
-        # print(clusters["vpe-subnets"])
-        # print(clusters["ingress-subnets"])
-        # print(clusters["bastion-subnets"])
-        # print(clusters["egress-subnets"])
-
-            
-        if type != "quickstart":
-            if type == "ibm-production" or type == "ibm-standard":
+        if "edge" not in type:
+            with Cluster("Cloud Services"):
+                services = list(set(services))  # get rid of duplicates
+                if "cos" in services and "ibm-object-storage" in services:
+                    services.remove("cos")
+                for serv in services:
+                    print(serv)
+                    n = platservice[serv](serv)
+                    nodes.update({serv: n})
+                servlink - Edge(color="blue") - nodes[next(iter(services))]
+        if 'quickstart' not in type:
+            if 'production' in type or 'standard' in type:
                 with Cluster("Consumer"):
-                    user = User("Users")
-                    internet = Cloudant("Internet")
-                user >> internet >> InternetServices("Cloud Internet Service") >> Bridge("Private LB") >> conslink
-            if type == "ibm-edge" or type == "ibm-standard":
+                    user = PeerCloud("Users")
+                    internet = Cis("Internet")
+                user >> internet >> Cdn("Cloud Internet Service") >> LocalLoadBalancing("Private LB") >> conslink
+            if 'edge' in type or 'standard' in type:
                 with Cluster("Remote Employee"):
-                    User("Remote employee") >> Cloudant() >> emplink
+                    PeerCloud("Remote employee") >> Cis() >> emplink
                 with Cluster("Enterprise Network"):
-                    dir = FileSync("Enterprise Directory")
-                    User("Enterprise User")
-                    EnterpriseApplications("Enterprise Application")
+                    dir = FileStorage("Enterprise Directory")
+                    PeerCloud("Enterprise User")
+                    Enterprise("Enterprise Application")
                 dirlink >> DirectLink("Direct link") >> dir
-                
-            
-
-
-
-            # create new cluster for each key
-            # for clust in clusters.keys():
-            #     makeClusters(clusters, clust)
-
 
     return send_file('/tmp/diagram.png', mimetype='image/png')
-
-
-# not being used right now
-def makeClusters(clusters, clust):
-    with Cluster(clust):
-        # include each node that belongs in the cluster
-        nodes = []
-        for node in clusters[clust]:
-            # if there are nested clusters, find those first
-            if (node in clusters.keys()):
-                # want to go to that cluster and get its nodes first
-                makeClusters(clusters, node)
-                clusters[node] = []
-            else:
-                newnode = GCS(node)
-                nodes.append(newnode)
-        for i in range(0,len(nodes) - 1):
-            nodes[i] - Edge(color="blue") - nodes[i + 1]
-    return 1
 
